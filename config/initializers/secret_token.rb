d1d27b57347500b2bc83df139b5d491282513d76
Ganapathi Manthiram <ganapathimanthiram@Ganapathi-Manthirams-MacBook.local>
2011-04-09 12:41:20 +0530

    First version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bojannam::Application.config.secret_token = '5161a75ea262055ff06aeb694a6e923999b0a7f623092b32cee3f2ef83629a6b2e2aa6ebd289054a0ea63cc6bd93fe1efce959a8517177b0adaaab65b58ac80d'
